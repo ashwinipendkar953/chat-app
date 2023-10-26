8d68d8d7fdbe29c333aab4eaa09b11db517f7ad8
Ashwini Pendkar <ashwinipendkar953@gmail.com>
2023-10-26 13:38:37 +0530

    manage user with profile context; put real time subs

--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,10 +1,19 @@
-import React from 'react';
 import { Redirect, Route } from 'react-router';
+import { useProfile } from '../context/profile.context';
+import { Container, Loader } from 'rsuite';
 
 const PublicRoute = ({ children, ...routeProps }) => {
-  const profile = false;
-  if (profile) {
-    return <Redirect to="/" />;
+  const { profile, isLoading } = useProfile();
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" content="Loading" speed="slow" />
+      </Container>
+    );
+  }
+
+  if (!profile && !isLoading) {
+    return <Redirect to="/signin" />;
   }
   return <Route {...routeProps}>{children}</Route>;
 };
